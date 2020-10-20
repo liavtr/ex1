e5e563a7a86b6c8666a1ed413a203a85e191d8c6
Liav Trabelsy <liavtra1@gmail.com>
2020-10-20 14:12:46 +0300

    added a main.cpp file

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by liavt on 20/10/2020.
+//
+
+#include "main.h"
+#include <iostream>
+int main() {
+    std::cout<<"Hello World!"<<endl;
+}
+
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by liavt on 20/10/2020.
+//
+
+#ifndef EX1_MAIN_H
+#define EX1_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //EX1_MAIN_H
